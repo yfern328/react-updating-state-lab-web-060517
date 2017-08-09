9e191cbd259147cbb7b97d7ec5d8c8031a9f4b7d
yfern328 <yfernan2@u.rochester.edu>
2017-08-09 01:29:34 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  clicked = () => {
+    this.setState({
+      timesClicked: ++this.state.timesClicked
+    })
+  }
+
+  render(){
+    return(
+      <div className="digital-clicker">
+        <button onClick={this.clicked}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+};
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,69 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  };
+
+  setBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12,
+      },
+    });
+  }
+
+  // OBJECT ASSIGN METHOD
+  // setBitrate = () => {
+  //   this.setState({
+  //     settings: Object.assign({}, this.state.settings, {
+  //       bitrate: 12
+  //     })
+  //   });
+  // }
+
+  setResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+          video: {
+            resolution: '720p',
+          }
+      },
+    });
+  }
+
+  // OBJECT ASSIGN METHOD
+  // setResolution = () => {
+  //   this.setState({
+  //     settings: Object.assign({}, this.state.settings, {
+  //       video: Object.assign({}, this.state.settings.video, {
+  //         resolution: '720p'
+  //       })
+  //     })
+  //   });
+  // }
+
+  render(){
+    return(
+      <div className="youtube-debugger">
+        <button className="bitrate" onClick={this.setBitrate}>bitrate</button>
+        <button className="resolution" onClick={this.setResolution}>resolution</button>
+      </div>
+    );
+  }
+};
+
+export default YouTubeDebugger;
